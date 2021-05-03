c964ed9b8c468edd5b7ce0eb2ef2c11f36b3f217
NKITTUSA <NKITTUSA@NKITTUSA-7410.idc.oracle.com>
2021-05-03 20:52:04 +0530

    Day-1 Docker Training

---
+++ cppDockerFiles/Hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+void func(){
+    while(true){
+        cout<<"Hey Nandha!\nWelcome to Docker Learning\n"<<endl;
+        sleep(10);
+    }
+}
+
+int main(){
+    func();
+    return 0;
+}
---
+++ pythonDockerFiles/hello.py
@@ -0,0 +1,9 @@
+import  time
+x="Hello Docker !!"
+
+while 3 > 2 :
+    print("Hey Nandha")
+    time.sleep(2)
+    print(x)
+    print("_____________++++++_____")
+    time.sleep(1)
